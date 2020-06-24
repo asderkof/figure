1eba2fae14ed058a66317d43fda6d7161c4e40a9
Ya Se Wang <yasewang@Yas-MacBook-Pro.local>
2020-06-23 20:59:53 -0400

    migration for tables

--- app/javascript/components/MenuLayout.jsx
+++ app/javascript/components/MenuLayout.jsx
@@ -14,16 +14,16 @@ export default function MenuLayout({children}) {
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
                     <Menu.Item key="1">Company Overview</Menu.Item>
                     <Menu.Item key="2">Employees</Menu.Item>
-                    <Menu.Item key="2">Upload Data</Menu.Item>
+                    <Menu.Item key="3">Upload Data</Menu.Item>
                 </Menu>
             </Header>
             <Layout>
                 <Sider width={200} className="site-layout-background">
                     <Menu
-                    mode="inline"
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    style={{ height: '100%', borderRight: 0 }}
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{ height: '100%', borderRight: 0 }}
                     >
                     <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
                         <Menu.Item key="1">option1</Menu.Item>
